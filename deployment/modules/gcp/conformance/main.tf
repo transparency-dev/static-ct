3d3c3c5630e0d147fea8867a5fc5f69df24ba434
Roger Ng <rogerng@google.com>
2024-11-13 15:28:44 +0000

    Refactor terraform modules

---
+++ deployment/modules/gcp/conformance/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "gcs" {}
+}
+
+module "storage" {
+  source = "../storage"
+
+  project_id = var.project_id
+  base_name  = var.base_name
+  location   = var.location
+}
+
+module "secretmanager" {
+  source = "../secretmanager"
+}
