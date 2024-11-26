44b77230e43e3d8c7eeee5d26d53fcc26e812da1
Roger Ng <rogerng@google.com>
2024-11-26 11:16:24 +0000

    Output GCP Cloud Run conformance URL (#66)

--- deployment/modules/gcp/conformance/outputs.tf
+++ deployment/modules/gcp/conformance/outputs.tf
@@ -7,3 +7,8 @@ output "ecdsa_p256_private_key_id" {
   description = "Signer private key (P256_SHA256)"
   value       = module.secretmanager.ecdsa_p256_private_key_id
 }
+
+output "conformance_url" {
+  description = "The URL of the running conformance server"
+  value       = module.cloudrun.conformance_url
+}
