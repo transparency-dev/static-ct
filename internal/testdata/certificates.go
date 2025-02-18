ae797daf2795bea23d4e48fc7a2e2037e519dacb
Philippe Boneff <phb@google.com>
2025-02-18 16:36:43 +0000

    reorder certs to match issuance path

--- internal/testdata/certificates.go
+++ internal/testdata/certificates.go
@@ -78,91 +78,6 @@ f1HWkLxHqd81TbD26yWVXeGJPE3VICskovPkQNJ0tU4b03YmnKliibduyqQQkOFP
 OwqULg==
 -----END CERTIFICATE-----`
 
-// FakeCACertPEM is a test CA cert for testing.
-//
-//	Data:
-//	    Version: 3 (0x2)
-//	    Serial Number:
-//	        b6:31:d2:ac:21:ab:65:20
-//	Signature Algorithm: sha256WithRSAEncryption
-//	    Issuer: C=GB, ST=London, L=London, O=Google, OU=Eng, CN=FakeCertificateAuthority
-//	    Validity
-//	        Not Before: Jul 11 12:23:26 2016 GMT
-//	        Not After : Jul 11 12:23:26 2017 GMT
-//	    Subject: C=GB, ST=London, L=London, O=Google, OU=Eng, CN=FakeCertificateAuthority
-//	    Subject Public Key Info:
-//	        Public Key Algorithm: rsaEncryption
-//	            Public-Key: (2048 bit)
-//	            Modulus:
-//	                00:a5:41:9a:7a:2d:98:a3:b5:78:6f:15:21:db:0c:
-//	                c1:0e:a1:f8:26:f5:b3:b2:67:85:dc:a1:e6:b7:83:
-//	                6d:da:63:da:d0:f6:a3:ff:bc:43:f5:2b:9f:00:19:
-//	                6e:6b:60:4b:43:20:6e:e2:cb:2e:b6:65:ed:9b:dc:
-//	                80:c3:e1:5a:96:af:60:78:0e:0e:fb:8f:ea:3e:3d:
-//	                c9:67:8f:a4:57:1c:ba:e4:f3:37:a9:2f:dd:11:9d:
-//	                10:5d:e5:d6:ef:d4:3b:06:d9:34:43:42:bb:bb:be:
-//	                43:40:2b:e3:b6:d1:b5:6c:58:12:34:96:14:d4:fc:
-//	                49:79:c5:26:8c:24:7d:b3:12:f5:f6:3e:b7:41:46:
-//	                6b:6d:3a:41:fd:7c:e3:b5:fc:96:6c:c6:cc:ad:8d:
-//	                48:09:73:44:64:ea:4f:17:1d:0a:4b:14:5a:19:07:
-//	                4a:32:0f:41:2e:e4:85:bd:a1:e1:9b:de:63:7c:3b:
-//	                bc:ec:aa:93:2a:0b:a8:c7:24:34:54:42:38:a5:d1:
-//	                0c:c4:f9:9e:7c:69:42:71:77:d7:95:aa:bb:13:3d:
-//	                f3:cc:c7:5d:b3:fd:76:25:25:e3:da:14:0e:59:81:
-//	                e8:2c:58:e8:09:29:7d:22:02:91:95:81:eb:55:6f:
-//	                2f:17:b9:af:4a:f3:84:8b:24:6e:ea:14:6b:bb:90:
-//	                84:35
-//	            Exponent: 65537 (0x10001)
-//	    X509v3 extensions:
-//	        X509v3 Subject Key Identifier:
-//	            01:02:03:04
-//	        X509v3 Authority Key Identifier:
-//	            keyid:01:02:03:04
-//
-//	        X509v3 Basic Constraints: critical
-//	            CA:TRUE, pathlen:10
-//	        X509v3 Key Usage: critical
-//	            Digital Signature, Non Repudiation, Key Encipherment, Data Encipherment, Key Agreement, Certificate Sign, CRL Sign, Encipher Only, Decipher Only
-//	Signature Algorithm: sha256WithRSAEncryption
-//	     92:be:33:eb:d5:d4:32:e7:9e:4e:65:2a:e8:3f:67:b8:f4:d7:
-//	     34:ab:95:11:6a:5d:ba:fd:57:9b:94:6e:8d:20:be:fb:7a:e1:
-//	     49:ca:39:ea:92:d3:81:5a:b1:87:a3:9f:50:a4:e0:1e:11:de:
-//	     c4:d1:07:a1:ca:d1:97:1a:92:bd:73:9a:11:ec:6a:9a:52:11:
-//	     2d:40:e1:3b:4f:3c:1f:81:3f:4c:ab:6a:02:84:4f:8b:18:36:
-//	     7a:cc:5c:a9:0e:25:2b:cd:57:53:88:d9:eb:82:b1:ce:62:76:
-//	     56:d4:23:9e:01:b3:6d:2b:49:ea:d4:3a:c2:f5:76:a7:b3:2d:
-//	     24:97:6f:b4:1c:74:6b:95:85:f6:b5:41:56:82:3c:ed:be:96:
-//	     1e:5e:6a:2d:7b:f7:fd:7d:6e:3f:fb:c2:ec:61:b3:7c:7f:3b:
-//	     f5:9c:64:61:5f:02:93:87:cd:81:f9:7e:53:3e:c1:f5:79:85:
-//	     f4:41:87:c7:ca:bd:af:ab:2b:a4:aa:a8:1d:2c:50:ad:23:8f:
-//	     db:13:1d:71:8a:85:bd:ac:59:6c:c4:53:c5:71:0c:90:91:f3:
-//	     0b:41:ef:da:6e:27:bb:09:57:9c:97:b9:d7:fc:20:96:c5:75:
-//	     96:ce:2e:6c:a8:b6:6e:b0:4d:0f:3e:01:95:ea:8b:cd:ae:47:
-//	     d0:d9:01:b7
-const FakeCACertPEM = `
------BEGIN CERTIFICATE-----
-MIIDrDCCApSgAwIBAgIJALYx0qwhq2UgMA0GCSqGSIb3DQEBCwUAMHExCzAJBgNV
-BAYTAkdCMQ8wDQYDVQQIDAZMb25kb24xDzANBgNVBAcMBkxvbmRvbjEPMA0GA1UE
-CgwGR29vZ2xlMQwwCgYDVQQLDANFbmcxITAfBgNVBAMMGEZha2VDZXJ0aWZpY2F0
-ZUF1dGhvcml0eTAeFw0xNjA3MTExMjIzMjZaFw0xNzA3MTExMjIzMjZaMHExCzAJ
-BgNVBAYTAkdCMQ8wDQYDVQQIDAZMb25kb24xDzANBgNVBAcMBkxvbmRvbjEPMA0G
-A1UECgwGR29vZ2xlMQwwCgYDVQQLDANFbmcxITAfBgNVBAMMGEZha2VDZXJ0aWZp
-Y2F0ZUF1dGhvcml0eTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKVB
-mnotmKO1eG8VIdsMwQ6h+Cb1s7Jnhdyh5reDbdpj2tD2o/+8Q/UrnwAZbmtgS0Mg
-buLLLrZl7ZvcgMPhWpavYHgODvuP6j49yWePpFccuuTzN6kv3RGdEF3l1u/UOwbZ
-NENCu7u+Q0Ar47bRtWxYEjSWFNT8SXnFJowkfbMS9fY+t0FGa206Qf1847X8lmzG
-zK2NSAlzRGTqTxcdCksUWhkHSjIPQS7khb2h4ZveY3w7vOyqkyoLqMckNFRCOKXR
-DMT5nnxpQnF315WquxM988zHXbP9diUl49oUDlmB6CxY6AkpfSICkZWB61VvLxe5
-r0rzhIskbuoUa7uQhDUCAwEAAaNHMEUwDQYDVR0OBAYEBAECAwQwDwYDVR0jBAgw
-BoAEAQIDBDASBgNVHRMBAf8ECDAGAQH/AgEKMA8GA1UdDwEB/wQFAwMH/4AwDQYJ
-KoZIhvcNAQELBQADggEBAJK+M+vV1DLnnk5lKug/Z7j01zSrlRFqXbr9V5uUbo0g
-vvt64UnKOeqS04FasYejn1Ck4B4R3sTRB6HK0Zcakr1zmhHsappSES1A4TtPPB+B
-P0yragKET4sYNnrMXKkOJSvNV1OI2euCsc5idlbUI54Bs20rSerUOsL1dqezLSSX
-b7QcdGuVhfa1QVaCPO2+lh5eai179/19bj/7wuxhs3x/O/WcZGFfApOHzYH5flM+
-wfV5hfRBh8fKva+rK6SqqB0sUK0jj9sTHXGKhb2sWWzEU8VxDJCR8wtB79puJ7sJ
-V5yXudf8IJbFdZbOLmyotm6wTQ8+AZXqi82uR9DZAbc=
------END CERTIFICATE-----`
-
 // PrecertPEMValid is a test certificate containing a valid CT precertificate
 // extension.
 //
@@ -291,6 +206,91 @@ A1UECBMFV2FsZXMxEDAOBgNVBAcTB0VydyBXZW6CAQAwCQYDVR0TBAIwADANBgkq
 hkiG9w0BAQUFAAOBgQAXHNhKrEFKmgMPIqrI9oiwgbJwm4SLTlURQGzXB/7QKFl6
 n678Lu4peNYzqqwU7TI1GX2ofg9xuIdfGsnniygXSd3t0Afj7PUGRfjL9mclbNah
 ZHteEyA7uFgt59Zpb2VtHGC5X0Vrf88zhXGQjxxpcn0kxPzNJJKVeVgU0drA5g==
+-----END CERTIFICATE-----`
+
+// FakeCACertPEM is a test CA cert for testing.
+//
+//	Data:
+//	    Version: 3 (0x2)
+//	    Serial Number:
+//	        b6:31:d2:ac:21:ab:65:20
+//	Signature Algorithm: sha256WithRSAEncryption
+//	    Issuer: C=GB, ST=London, L=London, O=Google, OU=Eng, CN=FakeCertificateAuthority
+//	    Validity
+//	        Not Before: Jul 11 12:23:26 2016 GMT
+//	        Not After : Jul 11 12:23:26 2017 GMT
+//	    Subject: C=GB, ST=London, L=London, O=Google, OU=Eng, CN=FakeCertificateAuthority
+//	    Subject Public Key Info:
+//	        Public Key Algorithm: rsaEncryption
+//	            Public-Key: (2048 bit)
+//	            Modulus:
+//	                00:a5:41:9a:7a:2d:98:a3:b5:78:6f:15:21:db:0c:
+//	                c1:0e:a1:f8:26:f5:b3:b2:67:85:dc:a1:e6:b7:83:
+//	                6d:da:63:da:d0:f6:a3:ff:bc:43:f5:2b:9f:00:19:
+//	                6e:6b:60:4b:43:20:6e:e2:cb:2e:b6:65:ed:9b:dc:
+//	                80:c3:e1:5a:96:af:60:78:0e:0e:fb:8f:ea:3e:3d:
+//	                c9:67:8f:a4:57:1c:ba:e4:f3:37:a9:2f:dd:11:9d:
+//	                10:5d:e5:d6:ef:d4:3b:06:d9:34:43:42:bb:bb:be:
+//	                43:40:2b:e3:b6:d1:b5:6c:58:12:34:96:14:d4:fc:
+//	                49:79:c5:26:8c:24:7d:b3:12:f5:f6:3e:b7:41:46:
+//	                6b:6d:3a:41:fd:7c:e3:b5:fc:96:6c:c6:cc:ad:8d:
+//	                48:09:73:44:64:ea:4f:17:1d:0a:4b:14:5a:19:07:
+//	                4a:32:0f:41:2e:e4:85:bd:a1:e1:9b:de:63:7c:3b:
+//	                bc:ec:aa:93:2a:0b:a8:c7:24:34:54:42:38:a5:d1:
+//	                0c:c4:f9:9e:7c:69:42:71:77:d7:95:aa:bb:13:3d:
+//	                f3:cc:c7:5d:b3:fd:76:25:25:e3:da:14:0e:59:81:
+//	                e8:2c:58:e8:09:29:7d:22:02:91:95:81:eb:55:6f:
+//	                2f:17:b9:af:4a:f3:84:8b:24:6e:ea:14:6b:bb:90:
+//	                84:35
+//	            Exponent: 65537 (0x10001)
+//	    X509v3 extensions:
+//	        X509v3 Subject Key Identifier:
+//	            01:02:03:04
+//	        X509v3 Authority Key Identifier:
+//	            keyid:01:02:03:04
+//
+//	        X509v3 Basic Constraints: critical
+//	            CA:TRUE, pathlen:10
+//	        X509v3 Key Usage: critical
+//	            Digital Signature, Non Repudiation, Key Encipherment, Data Encipherment, Key Agreement, Certificate Sign, CRL Sign, Encipher Only, Decipher Only
+//	Signature Algorithm: sha256WithRSAEncryption
+//	     92:be:33:eb:d5:d4:32:e7:9e:4e:65:2a:e8:3f:67:b8:f4:d7:
+//	     34:ab:95:11:6a:5d:ba:fd:57:9b:94:6e:8d:20:be:fb:7a:e1:
+//	     49:ca:39:ea:92:d3:81:5a:b1:87:a3:9f:50:a4:e0:1e:11:de:
+//	     c4:d1:07:a1:ca:d1:97:1a:92:bd:73:9a:11:ec:6a:9a:52:11:
+//	     2d:40:e1:3b:4f:3c:1f:81:3f:4c:ab:6a:02:84:4f:8b:18:36:
+//	     7a:cc:5c:a9:0e:25:2b:cd:57:53:88:d9:eb:82:b1:ce:62:76:
+//	     56:d4:23:9e:01:b3:6d:2b:49:ea:d4:3a:c2:f5:76:a7:b3:2d:
+//	     24:97:6f:b4:1c:74:6b:95:85:f6:b5:41:56:82:3c:ed:be:96:
+//	     1e:5e:6a:2d:7b:f7:fd:7d:6e:3f:fb:c2:ec:61:b3:7c:7f:3b:
+//	     f5:9c:64:61:5f:02:93:87:cd:81:f9:7e:53:3e:c1:f5:79:85:
+//	     f4:41:87:c7:ca:bd:af:ab:2b:a4:aa:a8:1d:2c:50:ad:23:8f:
+//	     db:13:1d:71:8a:85:bd:ac:59:6c:c4:53:c5:71:0c:90:91:f3:
+//	     0b:41:ef:da:6e:27:bb:09:57:9c:97:b9:d7:fc:20:96:c5:75:
+//	     96:ce:2e:6c:a8:b6:6e:b0:4d:0f:3e:01:95:ea:8b:cd:ae:47:
+//	     d0:d9:01:b7
+const FakeCACertPEM = `
+-----BEGIN CERTIFICATE-----
+MIIDrDCCApSgAwIBAgIJALYx0qwhq2UgMA0GCSqGSIb3DQEBCwUAMHExCzAJBgNV
+BAYTAkdCMQ8wDQYDVQQIDAZMb25kb24xDzANBgNVBAcMBkxvbmRvbjEPMA0GA1UE
+CgwGR29vZ2xlMQwwCgYDVQQLDANFbmcxITAfBgNVBAMMGEZha2VDZXJ0aWZpY2F0
+ZUF1dGhvcml0eTAeFw0xNjA3MTExMjIzMjZaFw0xNzA3MTExMjIzMjZaMHExCzAJ
+BgNVBAYTAkdCMQ8wDQYDVQQIDAZMb25kb24xDzANBgNVBAcMBkxvbmRvbjEPMA0G
+A1UECgwGR29vZ2xlMQwwCgYDVQQLDANFbmcxITAfBgNVBAMMGEZha2VDZXJ0aWZp
+Y2F0ZUF1dGhvcml0eTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKVB
+mnotmKO1eG8VIdsMwQ6h+Cb1s7Jnhdyh5reDbdpj2tD2o/+8Q/UrnwAZbmtgS0Mg
+buLLLrZl7ZvcgMPhWpavYHgODvuP6j49yWePpFccuuTzN6kv3RGdEF3l1u/UOwbZ
+NENCu7u+Q0Ar47bRtWxYEjSWFNT8SXnFJowkfbMS9fY+t0FGa206Qf1847X8lmzG
+zK2NSAlzRGTqTxcdCksUWhkHSjIPQS7khb2h4ZveY3w7vOyqkyoLqMckNFRCOKXR
+DMT5nnxpQnF315WquxM988zHXbP9diUl49oUDlmB6CxY6AkpfSICkZWB61VvLxe5
+r0rzhIskbuoUa7uQhDUCAwEAAaNHMEUwDQYDVR0OBAYEBAECAwQwDwYDVR0jBAgw
+BoAEAQIDBDASBgNVHRMBAf8ECDAGAQH/AgEKMA8GA1UdDwEB/wQFAwMH/4AwDQYJ
+KoZIhvcNAQELBQADggEBAJK+M+vV1DLnnk5lKug/Z7j01zSrlRFqXbr9V5uUbo0g
+vvt64UnKOeqS04FasYejn1Ck4B4R3sTRB6HK0Zcakr1zmhHsappSES1A4TtPPB+B
+P0yragKET4sYNnrMXKkOJSvNV1OI2euCsc5idlbUI54Bs20rSerUOsL1dqezLSSX
+b7QcdGuVhfa1QVaCPO2+lh5eai179/19bj/7wuxhs3x/O/WcZGFfApOHzYH5flM+
+wfV5hfRBh8fKva+rK6SqqB0sUK0jj9sTHXGKhb2sWWzEU8VxDJCR8wtB79puJ7sJ
+V5yXudf8IJbFdZbOLmyotm6wTQ8+AZXqi82uR9DZAbc=
 -----END CERTIFICATE-----`
 
 // FakeIntermediateCertPEM is a test intermediate CA cert.
