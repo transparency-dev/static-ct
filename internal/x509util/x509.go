94dee8aafb8d874d767741e941a73ea46401a349
Philippe Boneff <phb@google.com>
2025-02-25 15:48:22 +0000

    delete hasSanExtension

--- internal/x509util/x509.go
+++ internal/x509util/x509.go
@@ -1,26 +1,5 @@
 package x509util
 
-import (
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/asn1"
-)
-
 var (
 	oidExtensionSubjectAltName = []int{2, 5, 29, 17}
 )
-
-func hasSANExtension(c *x509.Certificate) bool {
-	return oidInExtensions(oidExtensionSubjectAltName, c.Extensions)
-}
-
-// oidInExtensions reports whether an extension with the given oid exists in
-// extensions.
-func oidInExtensions(oid asn1.ObjectIdentifier, extensions []pkix.Extension) bool {
-	for _, e := range extensions {
-		if e.Id.Equal(oid) {
-			return true
-		}
-	}
-	return false
-}
