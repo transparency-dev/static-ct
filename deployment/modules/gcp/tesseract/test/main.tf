1f1305633d788d133c7a4a75807b4ae7b87ae447
Roger Ng <rogerng@google.com>
2025-03-26 18:41:22 +0000

    Extract `tls_private_key` resource from `hashicorp/tls` provider into an insecure module

--- deployment/modules/gcp/tesseract/test/main.tf
+++ deployment/modules/gcp/tesseract/test/main.tf
@@ -14,5 +14,14 @@ module "storage" {
 module "secretmanager" {
   source = "../../secretmanager"
 
-  base_name = var.base_name
+  base_name                                  = var.base_name
+  tls_private_key_ecdsa_p256_public_key_pem  = module.insecuretlskey.tls_private_key_ecdsa_p256_public_key_pem
+  tls_private_key_ecdsa_p256_private_key_pem = module.insecuretlskey.tls_private_key_ecdsa_p256_private_key_pem
+}
+
+# [WARNING]
+# This module will hardcode unencrypted private keys in the Terraform state file.
+# DO NOT use this for production logs.
+module "insecuretlskey" {
+  source = "../../insecuretlskey"
 }
