11867bf8094b637dad4fa16d4de46a9df5a67d77
Roger Ng <rogerng@google.com>
2025-04-02 11:36:40 +0100

    [AWS] Extract `tls_private_key` resource from `hashicorp/tls` provider into an insecure module (#220)
    * Extract `tls_private_key` resource from `hashicorp/tls` provider into an insecure module
    
    * Address comment

--- deployment/modules/aws/secretsmanager/variables.tf
+++ deployment/modules/aws/secretsmanager/variables.tf
@@ -7,3 +7,15 @@ variable "region" {
   description = "Region in which to create resources"
   type        = string
 }
+
+variable "tls_private_key_ecdsa_p256_public_key_pem" {
+  description = "Public ECDSA key with P256 elliptic curve in PEM format."
+  type        = string
+  sensitive   = true
+}
+
+variable "tls_private_key_ecdsa_p256_private_key_pem" {
+  description = "Private ECDSA key with P256 elliptic curve in PEM format."
+  type        = string
+  sensitive   = true
+}
