31e1d54d937d73c4066f7e0465c52c55c2c45e5d
Roger Ng <rogerng@google.com>
2025-04-02 11:29:31 +0000

    Address comment

--- deployment/modules/gcp/tesseract/conformance/main.tf
+++ deployment/modules/gcp/tesseract/conformance/main.tf
@@ -20,7 +20,7 @@ module "secretmanager" {
 }
 
 # [WARNING]
-# This module will hardcode unencrypted private keys in the Terraform state file.
+# This module will store unencrypted private keys in the Terraform state file.
 # DO NOT use this for production logs.
 module "insecuretlskey" {
   source = "../../insecuretlskey"
