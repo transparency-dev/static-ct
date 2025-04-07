86b97f5cae86458c03861959293bc5a392526416
Philippe Boneff <phb@google.com>
2025-04-07 16:09:49 +0000

    print output in different step
    # Conflicts:
    #	deployment/modules/gcp/cloudbuild/tesseract/main.tf

--- deployment/modules/gcp/cloudbuild/tesseract/main.tf
+++ deployment/modules/gcp/cloudbuild/tesseract/main.tf
@@ -109,6 +109,23 @@ resource "google_cloudbuild_trigger" "build_trigger" {
       name   = "alpine/terragrunt"
       script = <<EOT
         terragrunt --terragrunt-non-interactive --terragrunt-no-color apply -auto-approve -no-color 2>&1
+      EOT
+      dir    = "deployment/live/gcp/static-ct-staging/logs/arche2025h1"
+      env = [
+        "GOOGLE_PROJECT=${var.project_id}",
+        "TF_IN_AUTOMATION=1",
+        "TF_INPUT=false",
+        "TF_VAR_project_id=${var.project_id}",
+        "DOCKER_CONTAINER_TAG=$SHORT_SHA"
+      ]
+      wait_for = ["docker_push_conformance_gcp"]
+    }
+
+    ## Print terraform output.
+    step {
+      id     = "terraform_print_output"
+      name   = "alpine/terragrunt"
+      script = <<EOT
         terragrunt --terragrunt-no-color output --raw conformance_url -no-color > /workspace/conformance_url
         terragrunt --terragrunt-no-color output --raw conformance_bucket_name -no-color > /workspace/conformance_bucket_name
         terragrunt --terragrunt-no-color output --raw ecdsa_p256_public_key_data -no-color > /workspace/conformance_log_public_key.pem
@@ -124,7 +141,7 @@ resource "google_cloudbuild_trigger" "build_trigger" {
         "TF_VAR_project_id=${var.project_id}",
         "DOCKER_CONTAINER_TAG=$SHORT_SHA"
       ]
-      wait_for = ["docker_push_conformance_gcp"]
+      wait_for = ["terraform_apply_conformance_staging"]
     }
 
     options {
