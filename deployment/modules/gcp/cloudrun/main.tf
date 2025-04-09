cacef6813b766d3e382d9764b15dac3c55fafe06
Roger Ng <rogerng@google.com>
2025-04-09 16:27:59 +0000

    Remove `metrics_endpoint` flag in GCP CI pipeline

--- deployment/modules/gcp/cloudrun/main.tf
+++ deployment/modules/gcp/cloudrun/main.tf
@@ -41,7 +41,6 @@ resource "google_cloud_run_v2_service" "default" {
         "--logtostderr",
         "--v=1",
         "--http_endpoint=:6962",
-        "--metrics_endpoint=:8080",
         "--bucket=${var.bucket}",
         "--spanner_db_path=${local.spanner_log_db_path}",
         "--spanner_dedup_db_path=${local.spanner_dedup_db_path}",
