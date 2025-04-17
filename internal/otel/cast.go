fbcd38f19b4e88ed530d3597dfa84e519bf5fd5b
Philippe Boneff <phb@google.com>
2025-04-17 16:12:40 +0000

    address comments

--- internal/otel/cast.go
+++ internal/otel/cast.go
@@ -17,8 +17,9 @@ package otel
 import "math"
 
 var (
-	// LatencyHistogramBuckets is a range of millisecond scale bucket boundaries which remain useful at around 1-2 seconds timescale in addition to smaller latencies.
-	LatencyHistogramBuckets = []float64{0, 10e-3, 50e-3, 100e-3, 200e-3, 300e-3, 400e-3, 500e-3, 600e-3, 700e-3, 800e-3, 900e-3, 1000e-3, 1200e-3, 1400e-3, 1600e-3, 1800e-3, 2000e-3, 2500e-3, 3000e-3, 4000e-3, 5000e-3, 6000e-3, 8000e-3, 10000e-3}
+	// SubSecondLatencyHistogramBuckets is a range of millisecond scale bucket boundaries which remain useful at around 1-2 seconds timescale in addition to smaller latencies.
+	// We use seconds as units, Open Telemetry's default unit for time.
+	SubSecondLatencyHistogramBuckets = []float64{0, 10e-3, 50e-3, 100e-3, 200e-3, 300e-3, 400e-3, 500e-3, 600e-3, 700e-3, 800e-3, 900e-3, 1000e-3, 1200e-3, 1400e-3, 1600e-3, 1800e-3, 2000e-3, 2500e-3, 3000e-3, 4000e-3, 5000e-3, 6000e-3, 8000e-3, 10000e-3}
 )
 
 // Clamp64 casts a uint64 to an int64, clamping it at MaxInt64 if the value is above.
