4dd94bff6fb8cd1e1ed360a08d32d9a73abd2b42
Philippe Boneff <phb@google.com>
2025-05-07 12:42:47 +0000

    spaces

--- deployment/modules/gcp/cloudbuild/preloader/main.tf
+++ deployment/modules/gcp/cloudbuild/preloader/main.tf
@@ -76,10 +76,10 @@ resource "google_cloudbuild_trigger" "preloader_trigger" {
 	      echo "Will run preloader between $START_INDEX and $END_INDEX"
         go run github.com/google/certificate-transparency-go/preload/preloader@master \
           --target_log_uri=${var.submission_url}/ \
-	        --target_bearer_token="$(cat /workspace/cb_identity)" \
+          --target_bearer_token="$(cat /workspace/cb_identity)" \
           --source_log_uri=${var.source_log_uri} \
-	        --start_index=$START_INDEX \
-	        --end_index=$END_INDEX \
+          --start_index=$START_INDEX \
+          --end_index=$END_INDEX \
           --num_workers=20 \
           --parallel_fetch=20 \
           --parallel_submit=20
